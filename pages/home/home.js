e0956c8981fddc624fb3e0d9e87934f716fe5c7e
taxtapy <taxtapyinc@gmail.com>
2026-01-05 11:54:21 -0300

    feat: Adding logic for the swapping of themes and background on the home page

--- pages/home/home.js
+++ pages/home/home.js
@@ -2,14 +2,24 @@ function setButtonState(button, isActive){
     const name = button.dataset.name
     const state = isActive ? "Select" : ""
 
-    button.src = `../../assets/home/Buttons/Top/${name}${state}${currentTheme}.png`
+    button.src = `../../assets/home/Buttons/Top/${currentTheme}/${name}${currentTheme}${state}.png`
 }
 
 let currentTheme = "Pink"
+let currentBackground = "LoveCity"
 
+const logo = document.getElementById("logo")
+const homeUi = document.getElementById("homeui")
 const buttons = document.querySelectorAll(".navbtn")
 
+logo.src = `../../assets/home/Logo/Logo${currentTheme}.png`
+homeUi.src = `../../assets/home/HomePage/Home${currentTheme}.png`
+document.body.style.backgroundImage = `url(../../assets/home/ChosenBackgrounds/${currentBackground}.png)`
+
 buttons.forEach(btn => {
+    //initially sets each button to have the correct theme color
+    btn.src = `../../assets/home/Buttons/Top/${currentTheme}/${btn.dataset.name}${currentTheme}.png`
+
     btn.addEventListener("mouseenter", () => {
         setButtonState(btn, true)
     })
