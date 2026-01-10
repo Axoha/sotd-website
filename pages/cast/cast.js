b5a4be9fe70561cd6a588c57ac503505d11606ed
taxtapy <taxtapyinc@gmail.com>
2026-01-10 16:07:07 -0300

    feat: adding carousel logic to the character entry template

--- pages/cast/cast.js
+++ pages/cast/cast.js
@@ -1,10 +1,10 @@
 import THEMES from "/assets/themeMap.js"
 
-let currentTheme = ""
+let currentTheme = "Green"
 
 const theme = THEMES[currentTheme]
 
-if(!theme) {
+if (!theme) {
     console.warn("Invalid theme, falling back to Pink")
 }
 
@@ -16,14 +16,14 @@ const resolvedBackground = safeTheme.defaultBg
 const logo = document.getElementById("logo")
 const homeUi = document.getElementById("ui")
 const buttons = document.querySelectorAll(".navbtn")
-const secretButtons = document.getElementById("secretButtons") 
+const secretButtons = document.getElementById("secretButtons")
 
 secretButtons.src = `/assets/home/Buttons/BottomSecret/SecretButtonRow${safeThemeName}.png`
 logo.src = `/assets/home/Logo/Logo${safeThemeName}.png`
 homeUi.src = `/assets/home/PageEmpty/RightSidebar/PageSidebar${safeThemeName}.png`
 document.body.style.backgroundImage = `url(../../assets/home/ChosenBackgrounds/${resolvedBackground}.png)`
 
-function setButtonState(button, isActive){
+function setButtonState(button, isActive) {
     const name = button.dataset.name
     const state = isActive ? "Select" : ""
 
@@ -46,3 +46,26 @@ buttons.forEach(btn => {
 const characterTitle = document.getElementById("characterTitle")
 
 characterTitle.style.color = safeTheme.accent
+
+//carousel stuff below
+
+const track = document.querySelector(".carouselTrack")
+const slides = track.children
+const nextBtn = document.getElementById("next")
+const prevBtn = document.getElementById("prev")
+
+let index = 0
+
+function updateCarousel() {
+    track.style.transform = `translateX(-${index * 100}%)`
+}
+
+nextBtn.addEventListener("click", () => {
+    index = (index + 1) % slides.length
+    updateCarousel()
+})
+
+prevBtn.addEventListener("click", () => {
+    index = (index - 1 + slides.length) % slides.length
+    updateCarousel()
+})
