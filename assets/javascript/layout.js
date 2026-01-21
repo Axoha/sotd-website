c7a737d7c862a85704442a07d1b23489833f79da
taxtapy <taxtapyinc@gmail.com>
2026-01-20 22:31:13 -0300

    feat: adding logiic to handle the WIP cursor image appearing and following the cursor when hovering over a data-wip element

--- assets/javascript/layout.js
+++ assets/javascript/layout.js
@@ -3,8 +3,30 @@ import { applyGlobalTheme } from "./theme/theme.js";
 
 function initLayout() {
     initNavBar()
-    //later it would be interesting to use localstorage here so your chosen theme doesn't reset to pink every time you switch pages
     applyGlobalTheme(localStorage.getItem(theme) || "Pink")
+}
+
+const wipCursor = document.getElementById("wipCursor");
+
+if (wipCursor) {
+    document.addEventListener("mousemove", (e) => {
+        wipCursor.style.left = `${e.clientX}px`;
+        wipCursor.style.top = `${e.clientY}px`;
+    });
+
+    document.addEventListener("mouseover", (e) => {
+        const target = e.target.closest("[data-wip]");
+        if (!target) return;
+
+        wipCursor.style.opacity = "1";
+    });
+
+    document.addEventListener("mouseout", (e) => {
+        const target = e.target.closest("[data-wip]");
+        if (!target) return;
+
+        wipCursor.style.opacity = "0";
+    });
 }
 
 initLayout()
