ed646dcfd96d0e14c321337e3daf9955cae0f765
taxtapy <taxtapyinc@gmail.com>
2026-01-20 23:11:46 -0300

    feat: creating the base structure for the nature grid page and it's entries

---
+++ pages/nature/grid.js
@@ -0,0 +1,16 @@
+import { renderGrid, setupAlphabetFilter } from "/assets/javascript/wiki/renderGrid.js"
+
+const container = document.getElementById("uiOverlay")
+const res = await fetch("./nature.index.json")
+let entries = await res.json()
+const options = {
+    urlPrefx: `/pages/nature/entries/index.html`,
+    thumbnailPath: `./thumbnails`
+}
+function render(list){
+renderGrid(container, list, options)
+}
+
+render(entries)
+
+setupAlphabetFilter(container, entries, render)
