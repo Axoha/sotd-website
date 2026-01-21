2f6d92b175255f0695576f22ee6999332a8db1cc
taxtapy <taxtapyinc@gmail.com>
2026-01-20 22:52:50 -0300

    refactor: using the new generic renderGrid function for the characters page as well for consistency

--- pages/cast/grid.js
+++ pages/cast/grid.js
@@ -1,46 +1,63 @@
-const overlay = document.getElementById("uiOverlay")
-const filterBar = document.getElementById("filterBar")
+import { renderGrid, setupAlphabetFilter } from "/assets/javascript/wiki/renderGrid.js"
 
+const container = document.getElementById("uiOverlay")
 const res = await fetch("./cast.index.json")
-const characters = await res.json()
-
-let allCharacters = [...characters]
-
-function renderCharacters(list) {
-    overlay.querySelectorAll(".castCard").forEach(c => c.remove())
-
-    list.forEach(char => {
-        const card = document.createElement("a")
-        card.className = "castCard"
-        card.href = `/pages/cast/characters/index.html?id=${char.id}`
-
-        card.innerHTML = `
-    <img src="./thumbnails/${char.thumbnail}">
-    <div class="sideContent">
-    <h3>${char.name}</h3>
-    <p>${char.flavorText}</p>
-    </div>
-    `
-
-        overlay.appendChild(card)
-    })
+let characters = await res.json()
+const options = {
+    urlPrefx: `/pages/cast/characters/index.html`,
+    thumbnailPath: `./thumbnails`
+}
+function render(list){
+renderGrid(container, list, options)
 }
 
-renderCharacters(allCharacters)
+render(characters)
 
-filterBar.addEventListener("click", e => {
-    const letter = e.target.dataset.letter
-    if (!letter) return
+setupAlphabetFilter(container, characters, render)
 
-    filterBar.querySelectorAll("span").forEach(s => s.classList.remove("active"))
-    e.target.classList.add("active")
+// const overlay = document.getElementById("uiOverlay")
+// const filterBar = document.getElementById("filterBar")
 
-    if (letter === "#") {
-        renderCharacters(allCharacters)
-    } else {
-        const filtered = allCharacters.filter(c => 
-            c.name.charAt(0).toUpperCase() === letter
-        )
-        renderCharacters(filtered)
-    }
-})
+// const res = await fetch("./cast.index.json")
+// const characters = await res.json()
+
+// let allCharacters = [...characters]
+
+// function renderCharacters(list) {
+//     overlay.querySelectorAll(".castCard").forEach(c => c.remove())
+
+//     list.forEach(char => {
+//         const card = document.createElement("a")
+//         card.className = "castCard"
+//         card.href = `/pages/cast/characters/index.html?id=${char.id}`
+
+//         card.innerHTML = `
+//     <img src="./thumbnails/${char.thumbnail}">
+//     <div class="sideContent">
+//     <h3>${char.name}</h3>
+//     <p>${char.flavorText}</p>
+//     </div>
+//     `
+
+//         overlay.appendChild(card)
+//     })
+// }
+
+// renderCharacters(allCharacters)
+
+// filterBar.addEventListener("click", e => {
+//     const letter = e.target.dataset.letter
+//     if (!letter) return
+
+//     filterBar.querySelectorAll("span").forEach(s => s.classList.remove("active"))
+//     e.target.classList.add("active")
+
+//     if (letter === "#") {
+//         renderCharacters(allCharacters)
+//     } else {
+//         const filtered = allCharacters.filter(c => 
+//             c.name.charAt(0).toUpperCase() === letter
+//         )
+//         renderCharacters(filtered)
+//     }
+// })
