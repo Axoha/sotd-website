3622c861418703e2e42b9fa99c72e1ab7a828981
taxtapy <taxtapyinc@gmail.com>
2026-01-26 23:03:52 -0300

    feat: adding a way to easily replace bumper image and destination to layout.js

--- assets/javascript/layout.js
+++ assets/javascript/layout.js
@@ -29,4 +29,9 @@ if (wipCursor) {
     });
 }
 
+const logoDiv = document.querySelector(".logo")
+const logoDivChildren = logoDiv.children
+logoDivChildren[1].href = "/pages/pardonourdust" //change this to change what page the bumper links to
+logoDivChildren[1].children[0].src = "/assets/bumper.png" //change this to change the image of the bumper, or just replace the file with the desired image ig, might need some css work 
+
 initLayout()
