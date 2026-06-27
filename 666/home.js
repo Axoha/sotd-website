0aba7dc045868f85ace5ec07fbca66a95b1dc1c2
AxoHa <erikaglaggle@gmail.com>
2026-06-28 01:57:48 +0200

    feat: added a temporary page for... things

---
+++ 666/home.js
@@ -0,0 +1,48 @@
+import { applyGlobalTheme } from "/assets/javascript/theme/theme.js"
+import { timeAgo } from "/assets/javascript/timeAgo.js"
+
+const res = await fetch("/assets/news/news.index.json")
+const news = await res.json()
+const uiContent = document.querySelector(".uiContent")
+
+news.slice(-4).reverse().forEach((article)=>{ //all these methods are so only the latest 4 news are displayed and so they're in order newest at the top oldest at the bottom
+    const articleDiv = document.createElement("div")
+    const articleLink = document.createElement("a")
+    const innerDiv = document.createElement("div")
+    const notifier = document.createElement("img")
+    const title = document.createElement("h2")
+    const body = document.createElement("p")
+    const timeAgoEl = document.createElement("p")
+
+    innerDiv.className = "articleContent"
+    articleDiv.className = "article"
+
+    articleLink.href = `./news/entry?id${article.id}`
+    articleLink.className = "articleLink"
+    
+    notifier.className = `${article.boxType.toLowerCase()} notifier`
+    notifier.dataset.box = article.boxType
+
+    title.innerHTML = article.title
+    title.className = "articleTitle"
+    body.innerHTML = article.body
+    body.className = "articleBody"
+
+    timeAgoEl.className = "timeAgo"
+    timeAgoEl.textContent = `Published ${timeAgo(article.publishTime)}`
+
+    const thumbnail = document.createElement("img")
+    thumbnail.className = "thumbnail"
+    thumbnail.src = `/assets/news/newsAssets/thumbnails/${article.thumbnail}`
+    
+    articleDiv.appendChild(articleLink)
+    articleLink.appendChild(notifier)
+    articleLink.appendChild(thumbnail)
+    articleLink.appendChild(title)
+    articleLink.appendChild(body)
+    articleLink.appendChild(timeAgoEl)
+
+    uiContent.appendChild(articleDiv)
+})
+
+applyGlobalTheme(localStorage.getItem("theme"))
